818c8c0c231c19c55a2acca25dd9c7e29b9e1368
silverage <sep.non2@gmail.com>
2023-12-10 03:56:15 +0900

    zip TEST

--- test/src/controllers/health.controller.js
+++
@@ -1,4 +0,0 @@
-
-export const healthController = (req, res, next) => {
-    res.send("HELLO, I'm Healthy!");
-};
--- test/src/routes/health.route.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { healthController } from "../controllers/health.controller";
-
-export const healthRoute = express.Router();
-
-healthRoute.get('', healthController)
