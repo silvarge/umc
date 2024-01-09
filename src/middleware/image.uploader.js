3cb315ba78aa37838c573d4552a2c04a4d723a64
silverage <sep.non2@gmail.com>
2024-01-09 16:15:35 +0900

    :art: [FEAT] 사진 업로드 UUID 부분 분리

--- src/middleware/image.uploader.js
+++ src/middleware/image.uploader.js
@@ -2,8 +2,7 @@ import AWS from 'aws-sdk'
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 
-// import { uuid } from './uuid.js';
-import { v4 } from 'uuid';
+import { createUUID } from './uuid.js';
 
 import path from 'path';
 import dotenv from 'dotenv';
@@ -18,13 +17,6 @@ const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_S3_ACCESS_KEY,
     secretAccessKey: process.env.AWS_S3_SECRET_KEY
 });
-
-const createUUID = () => {
-    const tokens = v4().split('-');
-    console.log("token", tokens);
-    return tokens[2] + tokens[1] + tokens[0] + tokens[3] + tokens[4];
-};
-
 
 // 확장자 검사 목록
 const allowedExtensions =  ['.png', '.jpg', '.jpeg', '.bmp', '.gif'];
